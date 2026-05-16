89d7d97257da5ab2e347b8f4f433b82059e3d8b1
thatdarkduck <57682736+thatdarkduck@users.noreply.github.com>
2026-05-16 15:45:33 +0200

    Sound Effects
    Sound Effects

--- GP2/FG25_GP2_Team7/Assets/Scripts/In level/LevelTransition.cs
+++ GP2/FG25_GP2_Team7/Assets/Scripts/In level/LevelTransition.cs
@@ -5,12 +5,14 @@ using UnityEngine.SceneManagement;
 public class LevelTransition : MonoBehaviour
 {
     [SerializeField] LevelEndStars StarUI;
+    [SerializeField] AudioSource WinSFX;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Trigger");
         if (other.CompareTag("Player"))
         {
+            WinSFX.Play();
             StarUI.LevelFinished = true;
             Time.timeScale = 0;
             this.GetComponent<Collider2D>().enabled = false;
--- GP2/FG25_GP2_Team7/Assets/Scripts/UI/Clock_Visual.cs
+++ GP2/FG25_GP2_Team7/Assets/Scripts/UI/Clock_Visual.cs
@@ -6,12 +6,16 @@ public class Clock_Visual : MonoBehaviour
     [SerializeField] LevelEndStars LevelEndStars;
     Slider slider;
     [SerializeField] GameObject Pointer;
+    private AudioSource Audio;
+    bool DoOnceBool = false;
+    bool AnotherStupidBool = false;
 
     float TimeLeft;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Audio = GetComponent<AudioSource>();
         slider = GetComponent<Slider>();
     }
 
@@ -23,11 +27,20 @@ public class Clock_Visual : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (!DoOnceBool && AnotherStupidBool)
+        {
+            DoOnceBool = true;
+            Audio.Play();
+        }
         if (Time.timeSinceLevelLoad <= LevelEndStars.MaxTimeGoal)
         {
             TimeLeft = Time.timeSinceLevelLoad / LevelEndStars.MaxTimeGoal;
             slider.value = TimeLeft;
             Pointer.transform.localRotation = Quaternion.Euler(0, 0, TimeLeft * 360);
+        }
+        else
+        {
+            AnotherStupidBool = true;
         }
     }
 }
